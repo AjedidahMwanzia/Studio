588e62ac98e7c21d485bf15ab000be96d379441b
AjedidahMwanzia <ajedidah.mwanzia@student.moringaschool.com>
2022-03-11 01:43:47 +0300

    style what we do section

---
+++ js/script.js
@@ -0,0 +1,17 @@
+
+$(document).ready(function(){
+    $(".visible  ").click(function(){
+    $(".hidden && .visible p").toggle()
+});
+    $(".hidden").click(function(){
+    $(".visible").toggle()
+})
+    $(".visible1").click(function(){
+    $(".hidden1").toggle()
+});
+    $(".visible2").click(function(){
+    $(".hidden2").toggle()
+});
+
+})
+
